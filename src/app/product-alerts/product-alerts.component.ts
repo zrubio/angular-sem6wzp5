098d6700cbd458cdfa921bac60cffbdfbbe3f6c5
zrubio <zrubio@protonmail.com>
2024-12-09 09:16:42 +0100

    First commit

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+
+  @Input() product;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
